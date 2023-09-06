fb26b8c76fb4045880cf66b3e3f8bb366fae821a
mohammadg <mohammad2rghdd@gmail.com>
2023-09-06 15:09:18 +0330

    making mui rtl

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
 import { Button } from "@mui/material";
 import "./App.css";
 
+//Create Custom Theme
+const theme = createTheme({
+  direction: "rtl",
+  typography: {
+    fontFamily: "vazir , roboto",
+  },
+});
+// Create RTL cache
+const cacheRTL = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
 function App() {
   return (
-    <div className="App">
-      <Button variant="contained">کلیک کنید</Button>
-    </div>
+    <CacheProvider value={cacheRTL}>
+      <ThemeProvider theme={theme}>
+        <HelmetProvider>
+          <Helmet>
+            <title>وب سایت سخصی محمد قلاوند</title>
+          </Helmet>
+          <div className="App">
+            <Button variant="contained">کلیک کن</Button>
+          </div>
+        </HelmetProvider>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
